deb6b95be3a68274bc330ef4c84d9efc3dd80c4c
LaisColetta <laiscoletta@gmail.com>
2022-02-17 20:16:57 +0000

    bmi.py

---
+++ code/week02/LaisWeek02/bmi.py
@@ -0,0 +1,10 @@
+
+
+from __future__ import division
+
+
+num1 =  input ('Enter your weight (kg): ')
+num2 = input ('Enter you height (cm): ')
+result = float((num1 /num2/num2)*10.000)
+print (result)
+
